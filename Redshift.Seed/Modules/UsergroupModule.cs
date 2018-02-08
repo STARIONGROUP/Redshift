4b4b35a326a3b9ec3e93312d1cdce5bb3f3fcab9
alexatrhea <a.vorobiev@rheagroup.com>
2018-02-08 15:06:55 +0100

    Added license headers.

--- Redshift.Seed/Modules/UsergroupModule.cs
+++ Redshift.Seed/Modules/UsergroupModule.cs
@@ -1,4 +1,29 @@
-﻿namespace Redshift.Seed.Modules
+﻿#region Copyright
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="UsergroupModule.cs" company="RHEA System S.A.">
+//    Copyright (c) 2018 RHEA System S.A.
+//
+//    Author: Alex Vorobiev
+//
+//    This file is part of Redshift.Seed.
+//
+//    Redshift.Seed is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Redshift.Seed is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Redshift.Seed.  If not, see <http://www.gnu.org/licenses/>.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+#endregion
+
+namespace Redshift.Seed.Modules
 {
     using System;
     using System.Collections.Generic;
