e042c88c32486d89ba05470e26111fe58adf821d
Alex Vorobiev <a.vorobiev@rheagroup.com>
2018-02-10 18:26:45 +0100

    Move sql setup to script

---
+++ Redshift.Orm.Tests/setup.sql
@@ -0,0 +1,52 @@
+﻿CREATE ROLE redshifttest WITH LOGIN PASSWORD '1234';
+ALTER DATABASE redshifttest OWNER TO redshifttest;
+
+GRANT ALL PRIVILEGES ON DATABASE redshifttest TO redshifttest;
+
+\connect redshifttest
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: public; Type: SCHEMA; Schema: -; Owner: redshifttest
+--
+
+CREATE SCHEMA IF NOT EXISTS public;
+
+ALTER SCHEMA public OWNER TO redshifttest;
+
+--
+-- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: redshifttest
+--
+
+COMMENT ON SCHEMA public IS 'standard public schema';
+
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+--
+-- Name: public; Type: ACL; Schema: -; Owner: redshifttest
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM redshifttest;
+GRANT ALL ON SCHEMA public TO redshifttest;
+GRANT ALL ON SCHEMA public TO PUBLIC;
