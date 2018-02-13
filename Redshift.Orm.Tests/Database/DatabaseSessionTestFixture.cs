b52d25ddcc06f3541ddb060728c4b9e02f81f433
alexatrhea <a.vorobiev@rheagroup.com>
2018-02-13 18:34:24 +0100

    Updated license header to contain the word Lesser to conform to LGPL guidelines

--- Redshift.Orm.Tests/Database/DatabaseSessionTestFixture.cs
+++ Redshift.Orm.Tests/Database/DatabaseSessionTestFixture.cs
@@ -5,20 +5,20 @@
 //
 //    Author: Alex Vorobiev
 //
-//    This file is part of Redshift.Orm.
+//    This file is part of Redshift.Orm.Tests.
 //
-//    Redshift.Orm is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
+//    Redshift.Orm.Tests is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Lesser General Public License as published by
 //    the Free Software Foundation, either version 3 of the License, or
 //    (at your option) any later version.
 //
-//    Redshift.Orm is distributed in the hope that it will be useful,
+//    Redshift.Orm.Tests is distributed in the hope that it will be useful,
 //    but WITHOUT ANY WARRANTY; without even the implied warranty of
 //    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
+//    GNU Lesser General Public License for more details.
 //
 //    You should have received a copy of the GNU General Public License
-//    along with Redshift.Orm.  If not, see <http://www.gnu.org/licenses/>.
+//    along with Redshift.Orm.Tests.  If not, see <http://www.gnu.org/licenses/>.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 #endregion
